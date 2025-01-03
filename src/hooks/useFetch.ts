cc103c3018c684651a4c4de0e12aa286010f5bab
JasrajChouhan <jasrajc945@gmail.com>
2025-01-03 11:02:10 +0530

    (feat): add useFetch hook for fetching data.

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+export const useFetch = (cb: (data: any) => Promise<any>) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState(null);
+
+  const fn = async (...args: any) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await cb(...args);
+      setData(data);
+    } catch (error: any) {
+      setError(error?.message as string);
+      toast.error(error?.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, error, data, fn };
+};
