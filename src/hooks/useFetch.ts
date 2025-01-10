9e43de9babeaf866e33d43a2987d2bfbaefb73ea
JasrajChouhan <jasrajc945@gmail.com>
2025-01-10 21:55:20 +0530

    (fix): user-fetch types in data.

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,7 +4,7 @@ import { toast } from 'sonner';
 export const useFetch = (cb: (data: any) => Promise<any>) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
 
   const fn = async (...args: any) => {
     setLoading(true);
