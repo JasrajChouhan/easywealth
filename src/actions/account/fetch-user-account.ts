34bd0ee4a64ead4b60de570690670c288474c6d1
JasrajChouhan <jasrajc945@gmail.com>
2025-01-10 21:59:29 +0530

    (chore): In fetch-user-account action.

--- src/actions/account/fetch-user-account.ts
+++ src/actions/account/fetch-user-account.ts
@@ -3,41 +3,33 @@
 import { db } from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
 
-const serializeTranction = (obj: { balance?: any; amount?: any }) => {
+const serializeTransaction = (obj) => {
   const serialized = { ...obj };
-
   if (obj.balance) {
     serialized.balance = obj.balance.toNumber();
   }
   if (obj.amount) {
     serialized.amount = obj.amount.toNumber();
   }
-
   return serialized;
 };
 
 export const fetchUserAccounts = async () => {
-  try {
-    const { userId } = await auth();
-    if (!userId) {
-      throw new Error('User ID is required');
-    }
-    const user = await db.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-    if (!user) {
-      throw new Error('User not found');
-    }
+  const { userId } = await auth();
+  if (!userId) throw new Error('Unauthorized');
 
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  try {
     const accounts = await db.account.findMany({
-      where: {
-        id: user.id,
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
+      where: { userId: user.id },
+      orderBy: { createdAt: 'desc' },
       include: {
         _count: {
           select: {
@@ -47,18 +39,11 @@ export const fetchUserAccounts = async () => {
       },
     });
 
-    if (!accounts) {
-      throw new Error('No accounts found');
-    }
+    // Serialize accounts before sending to client
+    const serializedAccounts = accounts.map(serializeTransaction);
 
-    // serlize the account
-    const serializeAccount = accounts.map(serializeTranction);
-
-    return {
-      success: true,
-      accounts: serializeAccount,
-    };
-  } catch (error: any) {
-    throw new Error(error.message);
+    return serializedAccounts;
+  } catch (error) {
+    console.error(error.message);
   }
 };
